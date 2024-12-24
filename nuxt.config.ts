bb887d54f5e2c9cbefdc8552ee09b3f3ae327837
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-24 15:55:51 +0900

    add ヘッダーのタイトルを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,4 +11,10 @@ export default defineNuxtConfig({
       'process.env.DEBUG': false,
     },
   },
+  app: {
+    head: {
+      title: '3D Library Tester',
+      meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    },
+  },
 })
