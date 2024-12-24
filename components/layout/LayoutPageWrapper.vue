24aaf0872f51a4e1f4c7a814a35733d3a221fd2b
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-24 15:56:50 +0900

    add ページのラッパーを作成

---
+++ components/layout/LayoutPageWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="LayoutPageWrapper">
+    <div class="PageWrapperInner">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.LayoutPageWrapper {
+  position: relative;
+  height: 100%;
+  width: 100%;
+  background-color: rgb(var(--v-theme-background));
+}
+</style>
