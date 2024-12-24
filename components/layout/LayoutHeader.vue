c4d0d3e36b72787b7be162771252173f8b5fa194
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-24 15:56:37 +0900

    add ヘッダーを作成

---
+++ components/layout/LayoutHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="LayoutHeader">
+    <div class="HeaderInner">
+      <div class="LeftSide">
+        <ButtonIcon class="mr-2" />
+        <h3>3D Library Tester</h3>
+      </div>
+
+      <div class="RightSide"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.LayoutHeader {
+  position: relative;
+  width: 100%;
+  height: 50px;
+  background-color: rgb(var(--v-theme-header));
+  border: 1px solid rgb(var(--v-theme-border));
+}
+
+.HeaderInner {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px 0 10px;
+}
+
+.LeftSide {
+  display: flex;
+}
+</style>
