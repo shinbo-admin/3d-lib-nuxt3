8730ef55f24da6398827e7cd5751aff800ec412e
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-24 15:56:15 +0900

    add ButtonIconを作成

---
+++ components/button/ButtonIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="ButtonIcon">
+    <div class="IconInner">
+      <v-icon :color="`${colors[color]}`" :icon="icon" :size="`${size}px`" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//------------------------------------------------------------------------------------------------------------
+// ページ設定・引数
+//------------------------------------------------------------------------------------------------------------
+interface Props {
+  icon?: string
+  size?: number
+  color?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  icon: 'mdi-cog-outline',
+  size: 30,
+  color: '#000',
+})
+
+//------------------------------------------------------------------------------------------------------------
+// 定数・変数（state）
+//------------------------------------------------------------------------------------------------------------
+const colors = useColor()
+</script>
+
+<style lang="scss">
+.ButtonIcon {
+}
+
+.IconInner {
+}
+</style>
