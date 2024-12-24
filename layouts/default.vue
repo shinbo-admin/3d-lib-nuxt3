fd305f6aaa540b97d372b88b5256bc773fa7b46b
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-24 12:43:09 +0900

    add レイアウトを追加

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="LayoutDefault"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+.LayoutDefault {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100dvh;
+  background-color: #fedefe;
+}
+</style>
