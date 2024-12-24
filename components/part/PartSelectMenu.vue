8244d9a7b0d8a689725f910615db382de52b2194
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-24 19:09:55 +0900

    add セレクトメニューを追加

---
+++ components/part/PartSelectMenu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="PartSelectMenu" :style="{ width: `${width}px` }">
+    <div class="SelectMenuInner">
+      <v-select :items="items" v-model="item" :density="height" @update:menu="onChange" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//------------------------------------------------------------------------------------------------------------
+// ページ設定・引数
+//------------------------------------------------------------------------------------------------------------
+interface Props {
+  items: string[]
+  width?: number
+  height?: 'default' | 'comfortable' | 'compact'
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const emit = defineEmits<{
+  (e: 'update:value', value: string): void
+  (e: 'update:error', value: string): void
+}>()
+
+//------------------------------------------------------------------------------------------------------------
+// 定数・変数（state）
+//------------------------------------------------------------------------------------------------------------
+const item = ref('')
+
+//------------------------------------------------------------------------------------------------------------
+// Function
+//------------------------------------------------------------------------------------------------------------
+function onChange() {
+  emit('update:value', item.value)
+}
+</script>
+
+<style lang="scss">
+.PartSelectMenu {
+  position: relative;
+}
+
+.SelectMenuInner {
+  position: relative;
+}
+</style>
