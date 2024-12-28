b0b91fb56987467ffc6a480471018f8c4535d0ce
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-28 14:30:07 +0900

    add フォームセレクトメニューを作成

---
+++ components/form/FormSelectMenu.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="FormSelectMenu">
+    <div class="SelectMenuInner">
+      <FormLabel :label="label" :isIcon="isIcon" :icon="icon" :width="width" />
+      <PartSelectMenu :items="contents" :width="width" :heightStr="heightStr" @update:value="onChange" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//------------------------------------------------------------------------------------------------------------
+// ページ設定・引数
+//------------------------------------------------------------------------------------------------------------
+interface Props {
+  label: string
+  isIcon?: boolean
+  icon?: string
+  contents: string[]
+  width?: number
+  heightStr?: 'default' | 'comfortable' | 'compact'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  heightStr: 'compact',
+})
+
+const emit = defineEmits<{}>()
+
+//------------------------------------------------------------------------------------------------------------
+// 定数・変数（state）
+//------------------------------------------------------------------------------------------------------------
+const item = ref('')
+
+//------------------------------------------------------------------------------------------------------------
+// Function
+//------------------------------------------------------------------------------------------------------------
+function onChange(value: string) {
+  console.log(value)
+}
+</script>
+
+<style lang="scss">
+.FormSelectMenu {
+  position: relative;
+}
+
+.SelectMenuInner {
+  position: relative;
+}
+</style>
