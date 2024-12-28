293d7b23e6f6303af53f831f9f2f220668d18d39
ShinsukeMaeda <sh.maeda@aiqveone.co.jp>
2024-12-28 15:18:20 +0900

    add ColorPickerを作成

---
+++ components/form/FormColorPicker.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="FormColorPicker">
+    <div class="ColorPickerInner">
+      <FormLabel :label="label" :isIcon="isIcon" :icon="icon" :width="width" />
+      <PartColorPicker :width="width" @update:value="onChange" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//------------------------------------------------------------------------------------------------------------
+// ページ設定・引数
+//------------------------------------------------------------------------------------------------------------
+interface Props {
+  label: string
+  isIcon?: boolean
+  icon?: string
+  width?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const emit = defineEmits<{}>()
+
+//------------------------------------------------------------------------------------------------------------
+// 定数・変数（state）
+//------------------------------------------------------------------------------------------------------------
+const color = ref('')
+
+//------------------------------------------------------------------------------------------------------------
+// Function
+//------------------------------------------------------------------------------------------------------------
+function onChange(value: string) {
+  color.value = value
+}
+</script>
+
+<style lang="scss">
+.FormColorPicker {
+  position: relative;
+}
+
+.ColorPickerInner {
+  position: relative;
+}
+</style>
---
+++ components/part/PartColorPicker.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="PartColorPicker" :style="{ width: `${width}px` }">
+    <div class="ColorPickerInner">
+      <input class="Color" type="color" v-model="color" @change="onChange" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//------------------------------------------------------------------------------------------------------------
+// ページ設定・引数
+//------------------------------------------------------------------------------------------------------------
+interface Props {
+  width?: number
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+
+const emit = defineEmits<{
+  (e: 'update:value', value: string): void
+  (e: 'update:error', value: string): void
+}>()
+
+//------------------------------------------------------------------------------------------------------------
+// 定数・変数（state）
+//------------------------------------------------------------------------------------------------------------
+const color = ref('')
+
+//------------------------------------------------------------------------------------------------------------
+// Function
+//------------------------------------------------------------------------------------------------------------
+function onChange() {
+  emit('update:value', color.value)
+}
+</script>
+
+<style lang="scss">
+.PartColorPicker {
+  position: relative;
+}
+
+.ColorPickerInner {
+  position: relative;
+  width: 100%;
+}
+
+.Color {
+  width: 100%;
+}
+</style>
